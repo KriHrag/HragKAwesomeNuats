09ea9f8ab64fbf48e983f683dfeafa0ce07da117
Hrag Krikorian <hrago18@gmail.com>
2015-03-21 17:34:26 -0700

    did some stuff...

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -14,9 +14,12 @@ game.PlayerEntity = me.Entity.extend({
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
         this.body.setVelocity(7, 20);
-
+        //keeps track of which direction your character is going
+        this.facing = "right";
+        
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 
         this.renderable.setCurrentAnimation("idle");
     },
@@ -26,10 +29,11 @@ game.PlayerEntity = me.Entity.extend({
             //setVelocity() and multiplying it by me.timer.tick
             // and me.timer.tick makes the moovement look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-            
+            this.facing = "right";
             this.flipX(true);
         }
         else if (me.input.isKeyPressed("left")) {
+            this.facing = "left";
             this.flipX(false);
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
 
@@ -39,7 +43,21 @@ game.PlayerEntity = me.Entity.extend({
             this.body.vel.x = 0;
         }
 
-        if(this.body.vel.x !== 0){
+ if(me.input.isKeyPressed("attack")){
+            console.log("attack1");
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log("attack2");
+                //sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack");
+                //makes it so that the next time we start the sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation
+                //this.renderable.setAnimationFrame();
+            }
+        }
+
+       else if(this.body.vel.x !== 0){
         if(!this.renderable.isCurrentAnimation("walk")){
             this.renderable.setCurrentAnimation("walk");
         }
@@ -47,7 +65,20 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
     }
      
-        
+         if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log(this.renderable.isCurrentAnimation("attack"));
+                //sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //makes it so that the next time we start the sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation
+                this.renderable.setAnimationFrame();
+            }
+        }
+
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
 
         if (me.input.isKeyPressed("up")) {
 
@@ -59,6 +90,20 @@ game.PlayerEntity = me.Entity.extend({
         
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    collideHandler: function(response){
+        if(response.b.type==='EnemyBaseEntity'){
+            var ydif = this.pos.y - response.b.pos.y;
+            var xdif = this.pos.x - response.b.pos.x;
+            
+            console.log("xdif " + xdif + " ydif " + ydif);
+            
+            if(xdif>-35){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x -1;
+            }
+        }
     }
 });
 
@@ -71,7 +116,7 @@ game.PlayerEntity = me.Entity.extend({
                 spritewidth: "100",
                 spriiteheight: "100",
                 getShape: function(){ 
-                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
             }]);
             this.broken = false;
@@ -112,7 +157,7 @@ game.PlayerEntity = me.Entity.extend({
                 spritewidth: "100",
                 spriiteheight: "100",
                 getShape: function(){ 
-                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
             }]);
             this.broken = false;
