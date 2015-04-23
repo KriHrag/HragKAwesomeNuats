910b403c5a205fe30f4cbfbfed42f7db499eddcd
Hrag Krikorian <hrago18@gmail.com>
2015-04-23 08:00:41 -0700

    something 123

---
+++ js/gamemanagers/GameManager.js
@@ -0,0 +1,65 @@
+game.GameTimerManager = Object.extend({
+    init: function(x, y, settings) {
+        this.now = new Date().getTime();
+        this.lastCreep = new Date().getTime();
+        this.paused = false;
+        this.alwaysUpdate = true;
+    },
+    update: function() {
+        this.now = new Date().getTime();
+
+        this.goldTimerCheck();
+
+        this.creepTimerCheck();
+
+
+
+        return true;
+    },
+    goldTimerCheck: function() {
+        if (Math.round(this.now / 1000) % 20 === 0 && (this.now - this.lastCreep >= 1000)) {
+            game.data.gold += (game.data.exp1+1);
+            console.log("Current gold: " + game.data.gold);
+        }
+    },
+    creepTimerCheck: function() {
+        if (Math.round(this.now / 1000) % 10 === 0 && (this.now - this.lastCreep >= 1000)) {
+            this.lastCreep = this.now;
+            var creepe = me.pool.pull("EnemyCreep", 500, 500, {});
+            me.game.world.addChild(creepe, 20);
+        }
+    }
+
+});
+
+
+
+game.ExperienceManager = Object.extend({
+    init: function(x, y, settings) {
+        this.alwaysUpdate = true;
+        this.gameover = false;
+    },
+    update: function() {
+        if (game.data.win === true && !this.gameover) {
+            this.gameOver(true);
+        } else if (game.data.win === false && !this.gameover) {
+           this.gameOver(false);
+        }
+
+        return true;
+    },
+    
+    gameOver: function(win){
+        if(win){
+            game.data.exp += 10;
+        }else{
+            game.data.exp += 1;
+        }
+        
+        this.gameover = true;
+        me.save.exp = game.data.exp;
+        
+    }
+
+});
+
